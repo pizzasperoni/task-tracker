87237779f9ca223107228260e4a640d29141fc60
Diego Speroni <diego.speroni@3sharecorp.com>
2021-03-09 12:34:48 -0300

    Initial commit - WIP: AddTask component & v-model

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header title="hello"/>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: [],
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if(confirm('sure?')){
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map(task=> task.id === id ? {...task, reminder: !task.reminder} : task)
+    }
+  },
+  created() {
+    // component did mount
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 2nd at 7:30pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 10th at 4:20pm',
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: 'Clean backyard',
+        day: 'March 3rd at 3:30pm',
+        reminder: true,
+      }
+    ]
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  -webkit-font-smoothing: ant ialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}  
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
 }
 </style>
